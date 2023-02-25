d0d99ab01f8d7fa627e67f068230a16a3f8b5702
= <=>
2023-02-25 18:59:14 +0900

    ✨ webpack dir-copy setting & install

--- webpack/build.js
+++ webpack/build.js
@@ -1,14 +1,16 @@
 const { merge } = require('webpack-merge');
 const { resolve } = require('node:path');
 const commonConfig = require('./common');
+const createCopyPlugin = require('./plugins/createCopyPlugin');
 
 const buildConfig = merge(commonConfig, {
   mode: 'production',
   devtool: false,
+  plugins: [...commonConfig.plugins, createCopyPlugin()].filter(Boolean),
   output: {
-    path: resolve(__dirname, '../build'), 
+    path: resolve(__dirname, '../build'),
     filename: '[name].min.js',
-  }
+  },
 });
 
 module.exports = buildConfig;
