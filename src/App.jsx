31e285ab92ee5929af511f69c04b17aba8709294
= <=>
2023-02-25 13:11:39 +0900

    ✨ webpack asset module install & setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import testPng from '../assets/test.png'
+
 
 const App = ({ message: initialMessage }) => {
   const [message] = useState(initialMessage ?? 'hello React *^^*');
 
+
   return (
     <div className="App">
       <h1>{message}</h1>
+      {testPng}
+      <img src={testPng}></img>
     </div>
   );
 };
--- webpack/common.js
+++ webpack/common.js
@@ -52,6 +52,17 @@ const commonConfig = {
         ],
         include: /\.module\.(css|s[ac]ss)$/i,
       },
+      // {
+      //   test: /\.(jpe?g|png|gif|webp|bmp)$/i,
+      //   type: 'asset/resource',
+      //   // 로더 별, 개별 설정
+      //   generator: {
+      //     filename: 'static/[name].[contenthash][ext][query]',
+      //   },
+      //   parser: {
+      //     dataUrlCondition: 8 * 1024, // 8kb
+      //   },
+      // },
     ],
   },
 };
