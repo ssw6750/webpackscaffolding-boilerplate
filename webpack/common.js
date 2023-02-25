a9261c955c36dcadfc7de6ef9d1d4568bf86513e
= <=>
2023-02-25 13:45:24 +0900

    🎨 build test & Fix svrg loader format

--- webpack/common.js
+++ webpack/common.js
@@ -63,6 +63,33 @@ const commonConfig = {
           dataUrlCondition: 8 * 1024, // 8kb
         },
       },
+      {
+        test: /\.svg$/i,
+        oneOf: [
+          {
+            dependency: { not: ['url'] },
+            use: [
+              {
+                loader: '@svgr/webpack',
+                options: {
+                  titleProp: true,
+                  svgo: true,
+                },
+              },
+              'new-url-loader',
+            ],
+          },
+          {
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/[name].[contenthash][ext][query]',
+            },
+            parser: {
+              dataUrlCondition: 4 * 1024,
+            },
+          },
+        ],
+      },
     ],
   },
 };
