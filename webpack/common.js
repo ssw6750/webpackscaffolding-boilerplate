3d7702d1fb357c69d7499477c4ef9377e5d52da5
= <=>
2023-02-25 11:27:54 +0900

    ✨webpack-css/style loader install & setting

--- webpack/common.js
+++ webpack/common.js
@@ -1,25 +1,37 @@
 const { resolve } = require("node:path");
 
 const commonConfig = {
-  target: ["web", "browserslist"],
-  resolve:{
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.wasm']
+  target: ['web', 'browserslist'],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.wasm'],
   },
   entry: {
-    main: resolve(__dirname, "../src/index.jsx"),
+    main: resolve(__dirname, '../src/index.jsx'),
   },
   module: {
     rules: [
       {
         // 어떤 파일을 변환할 것인가?
-        test:  /\.jsx?$/i,
+        test: /\.jsx?$/i,
         // 어떤 파일은 제외할 것인가?
         exclude: /node_modules/,
         // 어떤 로더를 사용해 파일을 변환할 것인가?
         use: 'babel-loader',
-      }
-    ]
-  }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
 
 // export default
