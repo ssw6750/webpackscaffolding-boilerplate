fb481d4aecf94694b681a85831b0dec8fc9093b5
= <=>
2023-02-26 20:30:47 +0900

    🔧 webpack css optimization setting

--- webpack/build.js
+++ webpack/build.js
@@ -3,6 +3,7 @@ const { resolve } = require('node:path');
 const commonConfig = require('./common');
 const createCopyPlugin = require('./plugins/createCopyPlugin');
 const { createCssExtractPlugin } = require('./plugins/createCssExtractPlugin');
+const createCssMinimizerPlugin = require('./plugins/createCssMinimizerPlugin');
 
 const buildConfig = merge(commonConfig, {
   mode: 'production',
@@ -12,6 +13,9 @@ const buildConfig = merge(commonConfig, {
     createCopyPlugin(),
     createCssExtractPlugin(),
   ].filter(Boolean),
+  optimization: {
+    minimizer: [createCssMinimizerPlugin()],
+  },
   output: {
     path: resolve(__dirname, '../build'),
     filename: '[name].min.js',
